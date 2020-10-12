d5a2cf6340f40017e50455b487b6e88eb4ae6b6d
uttne <uttne.mkr@gmail.com>
2020-10-13 00:27:32 +0900

    SQL をシンプルにした

--- SQL-Server/wait_db.sh
+++ SQL-Server/wait_db.sh
@@ -15,7 +15,7 @@ fi
 
 echo "wait connection"
 
-/opt/mssql-tools/bin/sqlcmd -S $SQLSERVER_HOST -U $SQLSERVER_USER -P $SQLSERVER_PASS -l 2 -Q "SELECT TOP 1 1 FOM sys.databases;" > /dev/null 2>&1
+/opt/mssql-tools/bin/sqlcmd -S $SQLSERVER_HOST -U $SQLSERVER_USER -P $SQLSERVER_PASS -l 2 -Q "SELECT 1;" > /dev/null 2>&1
 is_error=$?
 
 counter=1
@@ -24,7 +24,7 @@ while [ $is_error -gt 0 ]
 do
     sleep 1
     echo "retry connect. (${counter})"
-    /opt/mssql-tools/bin/sqlcmd -S $SQLSERVER_HOST -U $SQLSERVER_USER -P $SQLSERVER_PASS -l 2 -Q "SELECT TOP 1 1 FOM sys.databases;" > /dev/null 2>&1
+    /opt/mssql-tools/bin/sqlcmd -S $SQLSERVER_HOST -U $SQLSERVER_USER -P $SQLSERVER_PASS -l 2 -Q "SELECT 1;" > /dev/null 2>&1
     is_error=$?
 
     counter=$(( $counter + 1 ))
