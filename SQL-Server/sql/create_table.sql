1afe76ecbff25cfc07537ebe3685c05640828d8f
uttne <uttne.mkr@gmail.com>
2020-10-13 00:28:27 +0900

    テーブルを作成する SQL にコメントを追加する処理を統合

--- SQL-Server/sql/create_table.sql
+++ SQL-Server/sql/create_table.sql
@@ -13,3 +13,76 @@ CREATE TABLE [SampleDb].[dbo].[Post] (
     [content] nvarchar(max),
     FOREIGN KEY ([blog_id]) REFERENCES [SampleDb].[dbo].[Blog]([id])
 );
+
+
+-- Blog
+
+EXEC sys.sp_addextendedproperty  @name=N'MS_Description'
+                                ,@value=N'ブログ'
+                                ,@level0type=N'SCHEMA'
+                                ,@level0name=N'dbo'
+                                ,@level1type=N'TABLE'
+                                ,@level1name=N'Blog';
+
+EXEC sys.sp_addextendedproperty  @name=N'MS_Description'
+                                ,@value=N'ID'
+                                ,@level0type=N'SCHEMA'
+                                ,@level0name=N'dbo'
+                                ,@level1type=N'TABLE'
+                                ,@level1name=N'Blog'
+                                ,@level2type=N'COLUMN'
+                                ,@level2name=N'id';
+
+EXEC sys.sp_addextendedproperty  @name=N'MS_Description'
+                                ,@value=N'ブログのURL'
+                                ,@level0type=N'SCHEMA'
+                                ,@level0name=N'dbo'
+                                ,@level1type=N'TABLE'
+                                ,@level1name=N'Blog'
+                                ,@level2type=N'COLUMN'
+                                ,@level2name=N'url';
+
+-- Post
+
+EXEC sys.sp_addextendedproperty  @name=N'MS_Description'
+                                ,@value=N'ポスト'
+                                ,@level0type=N'SCHEMA'
+                                ,@level0name=N'dbo'
+                                ,@level1type=N'TABLE'
+                                ,@level1name=N'Post';
+
+EXEC sys.sp_addextendedproperty  @name=N'MS_Description'
+                                ,@value=N'ポストのID'
+                                ,@level0type=N'SCHEMA'
+                                ,@level0name=N'dbo'
+                                ,@level1type=N'TABLE'
+                                ,@level1name=N'Post'
+                                ,@level2type=N'COLUMN'
+                                ,@level2name=N'id';
+
+EXEC sys.sp_addextendedproperty  @name=N'MS_Description'
+                                ,@value=N'ブログのID'
+                                ,@level0type=N'SCHEMA'
+                                ,@level0name=N'dbo'
+                                ,@level1type=N'TABLE'
+                                ,@level1name=N'Post'
+                                ,@level2type=N'COLUMN'
+                                ,@level2name=N'blog_id';
+
+EXEC sys.sp_addextendedproperty  @name=N'MS_Description'
+                                ,@value=N'ポストのタイトル'
+                                ,@level0type=N'SCHEMA'
+                                ,@level0name=N'dbo'
+                                ,@level1type=N'TABLE'
+                                ,@level1name=N'Post'
+                                ,@level2type=N'COLUMN'
+                                ,@level2name=N'title';
+
+EXEC sys.sp_addextendedproperty  @name=N'MS_Description'
+                                ,@value=N'ポストの内容'
+                                ,@level0type=N'SCHEMA'
+                                ,@level0name=N'dbo'
+                                ,@level1type=N'TABLE'
+                                ,@level1name=N'Post'
+                                ,@level2type=N'COLUMN'
+                                ,@level2name=N'content';
