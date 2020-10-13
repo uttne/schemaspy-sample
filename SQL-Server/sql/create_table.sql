abb2889d3d169aaa44cdb646c21fe231c28e53a5
uttne <uttne.mkr@gmail.com>
2020-10-14 02:03:18 +0900

    id のコメントを他に合わせるために変更

--- SQL-Server/sql/create_table.sql
+++ SQL-Server/sql/create_table.sql
@@ -25,7 +25,7 @@ EXEC sys.sp_addextendedproperty  @name=N'MS_Description'
                                 ,@level1name=N'Blog';
 
 EXEC sys.sp_addextendedproperty  @name=N'MS_Description'
-                                ,@value=N'ID'
+                                ,@value=N'ブログのID'
                                 ,@level0type=N'SCHEMA'
                                 ,@level0name=N'dbo'
                                 ,@level1type=N'TABLE'
